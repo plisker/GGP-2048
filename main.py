6f22aae1787ba7aed0fe87b8e7ac9eb0554f6d94
Ahmed Ahmed <Welcome@tests-MacBook-Pro-4.local>
2016-12-08 08:44:23 -0500

    n/a

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+main.py
+
+by Ahmed Ahmed : ahmedahmed@college.harvard.edu
+   Paul Lisker : plisker@college.harvard.edu
+
+Main python file for our MCTS code. May be split up into separate files as the
+project grows.
+
+"""
+
+""" Abstract classes """
+class State ():
+
+class Node ():
